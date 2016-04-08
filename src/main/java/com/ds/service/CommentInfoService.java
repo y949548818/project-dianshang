aeaf214d47e0fcd779a8df5d9ccbcda10116ee5e
y949548818 <949548818@qq.com>
2016-04-08 10:33:28 +0800

    new

--- src/main/java/com/ds/service/CommentInfoService.java
+++ src/main/java/com/ds/service/CommentInfoService.java
@@ -1,11 +1,14 @@
 package com.ds.service;
 
+import com.ds.domain.User;
 
 /**
  * 暂时先用做评论的服务接口
  * @author qq245521957
  *
  */
-public interface CommentInfoService {
-
+public interface CommentInfoService{
+	String getCommentPublish();
+	int getCommentId();
+	User getCommentUser();
 }
