0a51a2ac4a0f882070e05a6af056861b574343f5
qq245521957 <245521957@qq.com>
2016-04-09 22:10:37 +0800

    修复了无法传送中文的bug

--- src/main/java/com/ds/controller/PostController.java
+++ src/main/java/com/ds/controller/PostController.java
@@ -1,5 +1,10 @@
 package com.ds.controller;
 
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,18 +29,29 @@ public class PostController {
 
 	@Autowired
 	PostServiceBean postService;
-	
-	
-//	@RequestMapping("/publish")
-//	public String publish(){
-//		return "publish";
-//	}
-	
-	@ResponseBody
-	@RequestMapping(value="/publish",method=RequestMethod.GET)
-	public String publish( String title,int postAdmin,String content){
-		Post post=new Post();
+
+
+	//	@RequestMapping("/publish")
+	//	public String publish(){
+	//		return "publish";
+	//	}
+	@RequestMapping(value="/publish",method=RequestMethod.POST,produces ="application/json;charset=UTF-8")
+	public void doPublish(@RequestParam(name="title",required=false,defaultValue="")String title,
+			@RequestParam(name="postAdmin",required=false,defaultValue="-1")int postAdmin,
+			@RequestParam(name="content",required=false,defaultValue="")String content,
+			PrintWriter writer){
+
 		ReturnStatus status=new ReturnStatus();
+		//		System.out.println("title:" + title);
+		//		System.out.println("postAdmin:" + postAdmin);
+		//		System.out.println("content:" + content);
+		if("".equals(title)||postAdmin==-1||"".equals(content)){
+			status.setReason("参数不全");
+			status.setResultKey(ReturnStatus.ERROR);
+			writer.write(JSON.toJSONString(status));
+			return;
+		}
+		Post post=new Post();
 		post.setTitle(title);
 		post.setPostAdmin(postAdmin);
 		post.setContent(content);
@@ -45,10 +61,11 @@ public class PostController {
 		else{
 			status.setResultKey(ReturnStatus.ERROR);
 		}
-		
-		return JSON.toJSONString(status);
+		//		writer.print(JSON.toJSONString(status));
+		writer.write(JSON.toJSONString(status));
+		return;
 	}
-	
-	
-	
+
+
+
 }
--- src/main/java/com/ds/controller/Test2Controller.java
+++ src/main/java/com/ds/controller/Test2Controller.java
@@ -1,5 +1,11 @@
 package com.ds.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +22,7 @@ import com.ds.service.UserInfoService;
  *
  */
 @Controller
+@RequestMapping(value="/test")
 public class Test2Controller {
 
 	@Autowired
@@ -26,6 +33,20 @@ public class Test2Controller {
 	public String test(){
 		
 		return "this is test";
+	}
+	@RequestMapping(value="/chinese",method=RequestMethod.GET)
+	public void chinese(PrintWriter writer) throws IOException{
+//			request.setCharacterEncoding("utf-8");
+//		response.setContentType("text/html;charset=UTF-8");
+//		response.getWriter().write("这是中文");
+		writer.write("这是中文");
+//		try {
+//			return new String("这是中文".getBytes("ISO-8859-1"),"ISO-8859-1");
+//		} catch (UnsupportedEncodingException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return "error";
 	}
 	
 	@ResponseBody
